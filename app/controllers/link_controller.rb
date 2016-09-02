d20fa07d44871c5de1ad834cd5ed14cb4f9f4381
kobe Lin <kobebryant721013@gmail.com>
2016-09-03 07:35:04 +0800

    1) add link controller[action:index, new(not finished)] 2) add some url rule in routes.rb

---
+++ app/controllers/link_controller.rb
@@ -0,0 +1,11 @@
+class LinkController < ApplicationController
+	
+	def index
+		puts "Link#index start"
+	end
+
+	def new
+		#@link = Link.new
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root "link#index" #顯示所有links
+	
+	resources :links do
+		resources :comments, only:[:index, :new, :create]
+	end
+
+	#resources :comments, only:[:new, :create] do
+
+	#end
+
+
+	#resources :articles do
+  #	resources :comments, only: [:index, :new, :create]
+	#end
+	#resources :comments, only: [:show, :edit, :update, :destroy]
 end
