a00287faddd50368def8af5acc0f9abac9a70d8b
kobe Lin <kobebryant721013@gmail.com>
2016-11-02 16:41:26 +0800

    1)新的連結在最上面 2)連結按下去會另開視窗

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,8 @@
 class Link < ApplicationRecord
   has_many :comments, -> {order(created_at: :desc)}
   belongs_to :user
-#  default_scope{ order(id: :desc) }
+  #新的link在最上面
+  default_scope{ order(id: :desc) }
 #p "link instance_methods:#{Link.instance_methods.sort}"
 
 
